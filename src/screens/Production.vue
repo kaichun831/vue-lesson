23b531e4280f6611327687ab065cd93da4858e1e
DESKTOP-P89PIVJ\KG <kaichun831@gmail.com>
2025-03-31 20:54:42 +0800

    refactor :　watch 的差異化

--- src/screens/Production.vue
+++ src/screens/Production.vue
@@ -14,17 +14,27 @@
   <button @click="setCookie">透過Cookie儲存資料</button>
   <button @click="getCookie">透過Cookie儲存資料</button>
   <h2>{{ $t('message.hello') }}</h2>
-  <button
+  <!-- <button
     @click="
       () => {
         i18n.global.locale = 'ja'
       }
     "
-  >
-    切換語言
-  </button>
+  > -->
+    <!-- 切換語言 -->
+  <!-- </button>
   <button @click="getLanguage">取得語言</button>
   <h2>{{ getUserLanguage }}</h2>
+
+  <h1>{{ object }}</h1>
+  <button @click="changeObject">Change Object</button> -->
+
+  <!-- <input v-model="userName"> -->
+
+   <!-- <button @click="changeObject">{{ listen.age }}</button> -->
+    <!-- <input v-model="oldPassword"> -->
+   <input v-model="newPassword">
+
 </template>
 
 <script setup>
@@ -33,13 +43,53 @@ import PageOne from './PageOne.vue'
 import PageTwo from './PageTwo.vue'
 import Cookies from 'js-cookie'
 import { i18n } from '@/main.js'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed, onUnmounted, watch } from 'vue'
 
 var tabs = [PageOne, PageTwo]
 const currentScreen = ref(0)
 const getLocalStoageValue = ref('')
 const getCookieValue = ref('')
 const getUserLanguage = ref('')
+const userName = ref('')
+
+const oldPassword = ref('qsc236578')
+const newPassword = ref('')
+
+
+
+
+
+
+// 瓶子 瓶子的名字叫做:寶礦力
+// 瓶子 瓶子的名字叫做:悅氏
+const listen = ref({
+  age:30,
+  name:"John"
+})
+const name = ref('KG')
+
+//deep true,用於監聽整個物件的內容變化
+watch(listen,(newValue,oldValue)=>{
+  if(newValue.age!=30){
+    alert('is not 30')
+  }
+},{deep:true})
+
+//物件內部監聽指定欄位參數
+watch(()=>listen.value.age,(newV,oldV)=>{
+  console.log(newV)
+  console.log(oldV)
+})
+//直接監聽基本型態
+watch(name,(newV,oldV)=>{
+   console.log(newV)
+  console.log(oldV)
+})
+
+function changeObject(){
+  listen.value.age = 50
+  name.value = "KAI"
+}
 
 onMounted(() => {
   getUserLanguage.value = getLanguage()
